e359a3ffcb148465d87e05de7936939ebae02d32
Haisam K. Ido <haisam.ido@nasa.gov>
2021-12-19 16:41:00 -0500

    initial steps to create db (wip)

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,59 +1,70 @@
 CREATE TABLE manufacturers (
-    manufacturer_id bigint NOT NULL,
+    manufacturer_id bigint NOT NULL PRIMARY KEY,
     manufacturer_name TEXT,
     comment TEXT
 );
 
 CREATE TABLE strings (
-    string_id bigint NOT NULL,
+    string_id bigint NOT NULL PRIMARY KEY,
     string_name TEXT,
+    string_year int,
     string_tensile_strength numeric,
-    string_guage numeric,
+    string_density numeric,
+    string_thickness numeric,
+    string_description TEXT,
     manufacturer_id bigint NOT NULL,
     comment TEXT
 );
 
+CREATE TABLE string_scales (
+    string_scale_id bigint NOT NULL PRIMARY KEY,
+    string_id bigint NOT NULL,
+    string_minimum_scale numeric,
+    string_maximum_scale numeric,
+    comment TEXT
+)
+
 CREATE attributes (
-    attribute_id,
+    attribute_id bigint NOT NULL PRIMARY KEY,
     attribute_name TEXT,
     comment TEXT
 );
 
 CREATE TABLE string_attributes (
-    string_attribute_id bigint NOT NULL,
+    string_attribute_id bigint NOT NULL PRIMARY KEY,
     string_id bigint NOT NULL,
     attribute_id bigint NOT NULL,
     comment TEXT
 );
 
 CREATE TABLE materials (
-    material_id bigint NOT NULL,
+    material_id bigint NOT NULL PRIMARY KEY,
     material_name TEXT,
     comment TEXT
 );
 
 CREATE TABLE string_materials (
-    string_material_id bigint NOT NULL,
+    string_material_id bigint NOT NULL PRIMARY KEY,
     string_id bigint NOT NULL,
     material_id bigint NOT NULL,
     comment TEXT
 );
 
 CREATE TABLE string_sets (
-    string_set_id bigint NOT NULL,
+    string_set_id bigint NOT NULL PRIMARY KEY,
     string_id bigint NOT NULL,
     string_set_name TEXT,
     comment TEXT
 );
 
 CREATE TABLE instruments (
-    instrument_id bigint NOT NULL,
+    instrument_id bigint NOT NULL PRIMARY KEY,
     instrument_name TEXT UNIQUE,
     comment TEXT
 );
 
 CREATE TABLE string_instruments (
-    string_instrument_id bigint NOT NULL,
+    string_instrument_id bigint NOT NULL PRIMARY KEY,
     string_id bigint,
     instrument_id bigint
     comment TEXT
