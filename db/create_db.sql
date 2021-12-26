704fe8c483124ae011d657f06b103c85b34efdb2
Haisam K. Ido <haisam.ido@nasa.gov>
2021-12-26 16:27:55 -0500

    altered instruments and strings tables

--- db/create_db.sql
+++ db/create_db.sql
@@ -33,78 +33,6 @@ CREATE TABLE units (
 );
 INSERT INTO units (unit,description) VALUES ( 'lb/in', 'pound mass per inch');
 
--- strings
-CREATE TABLE strings (
-    string_id SERIAL PRIMARY KEY,
-    manufacturer_id BIGINT NOT NULL, FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (manufacturer_id),
-    manufacturer_string_id TEXT NOT NULL,
-    density NUMERIC DEFAULT 0 NOT NULL,
-    unit_of_density NUMERIC DEFAULT 0 NOT NULL,
-    unit_weight NUMERIC DEFAULT 0 NOT NULL,
-    unit_of_unit_weight TEXT NOT NULL,FOREIGN KEY (unit_of_unit_weight) REFERENCES units (unit),
-    thickness NUMERIC DEFAULT 0 NOT NULL,
-    unit_of_thickness NUMERIC DEFAULT 0 NOT NULL,
-    tensile_strength NUMERIC DEFAULT 0 NOT NULL,
-    unit_of_tensile_strength NUMERIC DEFAULT 0 NOT NULL,
-    source TEXT DEFAULT '' NOT NULL,
-    UNIQUE(manufacturer_id, manufacturer_string_id, density, unit_weight,unit_of_unit_weight,thickness,unit_of_thickness,tensile_strength,unit_of_tensile_strength),
-    description TEXT,
-    comment TEXT
-);
-INSERT INTO strings(manufacturer_id, manufacturer_string_id, unit_weight, unit_of_unit_weight, source ) VALUES (
-    (SELECT manufacturer_id from manufacturers where manufacturer_name='D''Addario' AND manufacturer_type='strings'),
-    'J4301',.00002024,'lb/in',
-    'https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf'
-);
-INSERT INTO strings(manufacturer_id, manufacturer_string_id, unit_weight, unit_of_unit_weight, source ) VALUES ( (SELECT manufacturer_id from manufacturers where manufacturer_name='D''Addario' AND manufacturer_type='strings'),
-    'J4302',.00002729,'lb/in',
-    'https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf'
-);
-
-CREATE TABLE strings2 (
-    string_id SERIAL PRIMARY KEY,
-    manufacturer_id BIGINT NOT NULL, FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (manufacturer_id),
-    manufacturer_string_id TEXT NOT NULL,
-    instrument_id BIGINT NOT NULL, FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id),
-    unit_weight NUMERIC DEFAULT 0 NOT NULL,
-    unit_of_unit_weight TEXT NOT NULL,FOREIGN KEY (unit_of_unit_weight) REFERENCES units (unit),
-    source TEXT DEFAULT '' NOT NULL,
-    UNIQUE(manufacturer_id, manufacturer_string_id, unit_weight, unit_of_unit_weight),
-    description TEXT,
-    comment TEXT
-);
-INSERT INTO strings2(manufacturer_id, manufacturer_string_id, unit_weight, unit_of_unit_weight, instrument_id,source ) VALUES ( 
-    (SELECT manufacturer_id from manufacturers where manufacturer_name='D''Addario' AND manufacturer_type='strings'),
-    'J4302',
-    .00002729,
-    'lb/in',
-    array(select * from instruments where name='guitar' and type='stringed' and category='classical'),
-    'https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf'
-);
-
--- string sets
-CREATE TABLE string_sets (
-    string_set_id SERIAL PRIMARY KEY,
-    string_set_name TEXT NOT NULL,
-	manufacturer_id BIGINT NOT NULL, FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (manufacturer_id),
-    number_of_strings int NOT NULL,
-    UNIQUE(string_set_name,number_of_strings),
-    UNIQUE(manufacturer_id,string_set_name,number_of_strings),
-    comment TEXT
-);
-INSERT INTO string_sets (manufacturer_id, string_set_name, number_of_strings ) VALUES (
-    (SELECT manufacturer_id from manufacturers where manufacturer_name='D''Addario' AND manufacturer_type='strings'),
-    'EXL-120',
-    6);
-
--- CREATE TABLE strings_in_sets (
---     string_id BIGINT NOT NULL, FOREIGN KEY (string_id) REFERENCES strings (string_id),
---     string_set_name TEXT NOT NULL, FOREIGN KEY (string_set_name) REFERENCES string_sets (string_set_name),
---     string_order int NOT NULL,
---     UNIQUE(string_id,string_set_name,string_order),
---     comment TEXT
--- );
-
 --- instruments
 -- TODO: there are stringed instruments that are not plucked! violin etc. so perhaps stringed should be plucked-string ?
 CREATE TABLE instrument_types (
@@ -129,63 +57,119 @@ INSERT INTO instrument_categories (instrument_category) VALUES ( 'acoustic' );
 INSERT INTO instrument_categories (instrument_category) VALUES ( 'classical' );
 INSERT INTO instrument_categories (instrument_category) VALUES ( 'solid-body' );
 
-CREATE TABLE adjustable_scale_length_types (
-    id SERIAL PRIMARY KEY,  
-    type TEXT NOT NULL UNIQUE,
-    description TEXT NULL DEFAULT '',
-    comment TEXT
-);
-INSERT INTO adjustable_scale_length_types (type,description) VALUES ( 'NA', 'not applicable' );
-INSERT INTO adjustable_scale_length_types (type) VALUES ( 'fret' );
-INSERT INTO adjustable_scale_length_types (type) VALUES ( 'fretless' );
-
 CREATE TABLE instruments (
     instrument_id SERIAL PRIMARY KEY,
+    type TEXT NOT NULL, FOREIGN KEY (type) REFERENCES instrument_types (type),
     name TEXT NOT NULL,
     category TEXT NOT NULL, FOREIGN KEY (category) REFERENCES instrument_categories (instrument_category),
-    subcategory TEXT NOT NULL DEFAULT '',
-    type TEXT NOT NULL, FOREIGN KEY (type) REFERENCES instrument_types (type),
-    style TEXT NOT NULL DEFAULT '',
-    number_of_actuators INT NOT NULL,
-    electric BOOLEAN NOT NULL DEFAULT 'false',
-    adjustable_scale_length BOOLEAN NOT NULL DEFAULT 'true',
-    adjustable_scale_length_type TEXT NOT NULL DEFAULT 'fret', FOREIGN KEY (adjustable_scale_length_type) REFERENCES adjustable_scale_length_types (type),
-    multi_scale BOOLEAN NOT NULL DEFAULT 'false',
-    comment TEXT,
-    UNIQUE(name,category,subcategory,type,style,number_of_actuators,electric,adjustable_scale_length,adjustable_scale_length_type,multi_scale)
-);
-INSERT INTO instruments (type,name,category,style,number_of_actuators) VALUES ('stringed','guitar','classical','spanish',6);
-INSERT INTO instruments (type,name,category,style,number_of_actuators,adjustable_scale_length_type) VALUES ('stringed','guitar','classical','spanish',6,'fretless');
-INSERT INTO instruments (type,name,category,style,number_of_actuators,multi_scale) VALUES ('stringed','guitar','classical','spanish',6,'true');
-INSERT INTO instruments (type,name,category,style,number_of_actuators,electric) VALUES ('stringed','guitar','classical','spanish',6,'true');
-
-
-CREATE TABLE instrument_attributes (
-    id SERIAL PRIMARY KEY,
-    key TEXT NOT NULL,
-    value TEXT NOT NULL,
-    instrument_id BIGINT NOT NULL, FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id),
-    UNIQUE(name, instrument_id),
+    UNIQUE(type,name,category),
     description TEXT,
     comment TEXT
 );
-INSERT INTO instrument_attributes (key,value,instrument_id) VALUES (
+INSERT INTO instruments (type,name,category) VALUES ('stringed','guitar','classical');
+
+CREATE TABLE instrument_attributes (
+    id SERIAL PRIMARY KEY,
+    instrument_attribute TEXT NOT NULL UNIQUE,
+    instrument_attribute_type TEXT NOT NULL,
+    description TEXT,
+    comment TEXT  
+);
+INSERT INTO instrument_attributes (instrument_attribute,instrument_attribute_type) VALUES ('number_of_actuators', 'INT');
+INSERT INTO instrument_attributes (instrument_attribute,instrument_attribute_type) VALUES ('ethnic_association','TEXT');
+INSERT INTO instrument_attributes (instrument_attribute,instrument_attribute_type) VALUES ('electric','BOOLEAN');
+INSERT INTO instrument_attributes (instrument_attribute,instrument_attribute_type) VALUES ('multi_scale','BOOLEAN');
+INSERT INTO instrument_attributes (instrument_attribute,instrument_attribute_type) VALUES ('adjustable_scale_length','BOOLEAN');
+INSERT INTO instrument_attributes (instrument_attribute,instrument_attribute_type) VALUES ('adjustable_scale_length_type','TEXT');
+
+CREATE TABLE instrument_characteristics (
+    id SERIAL PRIMARY KEY,
+    instrument_id BIGINT NOT NULL, FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id),
+    instrument_characteristic TEXT NOT NULL, FOREIGN KEY (instrument_characteristic) REFERENCES instrument_attributes (instrument_attribute),
+    instrument_characteristic_value TEXT NOT NULL,
+    UNIQUE(instrument_id,instrument_characteristic,instrument_characteristic_value),
+    description TEXT,
+    comment TEXT
+);
+INSERT INTO instrument_characteristics (instrument_id,instrument_characteristic,instrument_characteristic_value) VALUES (
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
     'number_of_actuators',
-    6,
-    (SELECT instrument_id where type='stringed' and name='guitar' and category='classical')
+    6
+);
+INSERT INTO instrument_characteristics (instrument_id,instrument_characteristic,instrument_characteristic_value) VALUES (
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
+    'number_of_actuators',
+    7
+);
+INSERT INTO instrument_characteristics (instrument_id,instrument_characteristic,instrument_characteristic_value) VALUES (
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
+    'number_of_actuators',
+    8
+);
+INSERT INTO instrument_characteristics (instrument_id,instrument_characteristic,instrument_characteristic_value) VALUES (
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
+    'number_of_actuators',
+    9
+);
+INSERT INTO instrument_characteristics (instrument_id,instrument_characteristic,instrument_characteristic_value) VALUES (
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
+    'number_of_actuators',
+    12
 );
 
--- INSERT INTO instruments (name,category,type,number_of_actuators) VALUES ('guitar','classical','stringed',6);
--- INSERT INTO instruments (name,category,type,number_of_actuators) VALUES ('guitar','classical-electric','stringed',6);
--- INSERT INTO instruments (name,category,type,number_of_actuators) VALUES ('guitar','acoustic','stringed',6);
--- INSERT INTO instruments (name,category,type,number_of_actuators) VALUES ('guitar','acoustic-electric','stringed',6);
--- INSERT INTO instruments (name,category,type,number_of_actuators,fretted) VALUES ('guitar','classical','stringed',6,'false');
--- INSERT INTO instruments (name,category,type,style,number_of_actuators) VALUES ('guitar','classical','stringed','brazillian',7);
--- INSERT INTO instruments (name,category,type,style,number_of_actuators) VALUES ('guitar','classical','stringed','brazillian',8);
--- INSERT INTO instruments (name,category,type,style,number_of_actuators) VALUES ('guitar','classical','stringed','russian',7);
--- INSERT INTO instruments (name,category,type,number_of_actuators) VALUES ('lute guitar','classical','stringed',6);
--- INSERT INTO instruments (name,category,type,style,number_of_actuators) VALUES ('lute guitar','classical','stringed','german',6);
--- INSERT INTO instruments (name,category,type,style,number_of_actuators) VALUES ('banjo','acoustic','stringed','american',5);
+-- strings
+CREATE TABLE strings (
+    string_id SERIAL PRIMARY KEY,
+    manufacturer_id BIGINT NOT NULL, FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (manufacturer_id),
+    manufacturer_string_id TEXT NOT NULL,
+    instrument_id BIGINT NOT NULL, FOREIGN KEY (instrument_id) REFERENCES instruments (instrument_id),
+    unit_weight NUMERIC DEFAULT 0 NOT NULL,
+    unit_of_unit_weight TEXT NOT NULL,FOREIGN KEY (unit_of_unit_weight) REFERENCES units (unit),
+    source TEXT DEFAULT '' NOT NULL,
+    UNIQUE(manufacturer_id, manufacturer_string_id,unit_weight,unit_of_unit_weight),
+    description TEXT,
+    comment TEXT
+);
+INSERT INTO strings(manufacturer_id,manufacturer_string_id,unit_weight,unit_of_unit_weight,instrument_id,source ) VALUES ( 
+    (SELECT manufacturer_id FROM manufacturers WHERE manufacturer_name='D''Addario' AND manufacturer_type='strings'),
+    'J4301',
+    .00002024,
+    'lb/in',
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
+    'https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf'
+);
+INSERT INTO strings(manufacturer_id,manufacturer_string_id,unit_weight,unit_of_unit_weight,instrument_id,source ) VALUES ( 
+    (SELECT manufacturer_id FROM manufacturers WHERE manufacturer_name='D''Addario' AND manufacturer_type='strings'),
+    'J4302',
+    .00002729,
+    'lb/in',
+    (SELECT instrument_id FROM instruments WHERE type='stringed' AND name='guitar' AND category='classical'),
+    'https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf'
+);
+
+-- string sets
+CREATE TABLE string_sets (
+    string_set_id SERIAL PRIMARY KEY,
+    string_set_name TEXT NOT NULL,
+	manufacturer_id BIGINT NOT NULL, FOREIGN KEY (manufacturer_id) REFERENCES manufacturers (manufacturer_id),
+    number_of_strings int NOT NULL,
+    UNIQUE(string_set_name,number_of_strings),
+    UNIQUE(manufacturer_id,string_set_name,number_of_strings),
+    comment TEXT
+);
+INSERT INTO string_sets (manufacturer_id, string_set_name, number_of_strings ) VALUES (
+    (SELECT manufacturer_id FROM manufacturers WHERE manufacturer_name='D''Addario' AND manufacturer_type='strings'),
+    'EXL-120',
+    6);
+
+-- CREATE TABLE strings_in_sets (
+--     string_id BIGINT NOT NULL, FOREIGN KEY (string_id) REFERENCES strings (string_id),
+--     string_set_name TEXT NOT NULL, FOREIGN KEY (string_set_name) REFERENCES string_sets (string_set_name),
+--     string_order int NOT NULL,
+--     UNIQUE(string_id,string_set_name,string_order),
+--     comment TEXT
+-- );
+
 
 -- materials
 CREATE TABLE material_types (
