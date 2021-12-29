e977eb979e7dbcd592f4ed991cfec7039ad5b118
Haisam K. Ido <haisam.ido@nasa.gov>
2021-12-29 11:31:56 -0500

    added function public.music_string_mass_per_length and confirmed results against https://www.daddario.com/globalassets/pdfs/accessories/tension_chart_13934.pdf

--- db/create_db.sql
+++ db/create_db.sql
@@ -213,15 +213,15 @@ CREATE TABLE string_attributes (
 -- music
 -- https://en.wikipedia.org/wiki/String_vibration
 -- http://www.donaldsauter.com/string-calculation.htm
-DROP FUNCTION IF EXISTS public.string_frequency(numeric, numeric, numeric, numeric, numeric);
-CREATE FUNCTION string_frequency(
-    L NUMERIC,
+DROP FUNCTION IF EXISTS public.music_string_frequency(numeric, numeric, numeric, numeric, numeric);
+CREATE FUNCTION music_string_frequency(
+    length NUMERIC,
     density NUMERIC,
     diameter NUMERIC,
     tension NUMERIC,
     n NUMERIC DEFAULT 1
 ) RETURNS numeric
-	AS 'SELECT ((n/(L*diameter))*sqrt(tension/(PI()*density)));'
+	AS 'SELECT ((n/(length*diameter))*sqrt(tension/(PI()*density)));'
     LANGUAGE SQL
     IMMUTABLE
     RETURNS NULL ON NULL INPUT;
@@ -241,6 +241,59 @@ CREATE FUNCTION music_frequency_of_interval(
 SELECT music_frequency_of_interval(f0=>440.0,intervals=>12, n=>0);
 SELECT music_frequency_of_interval(f0=>440.0, n=>1);
 SELECT music_frequency_of_interval(440.0,12,1);
+
+-- https://en.wikipedia.org/wiki/String_vibration
+DROP FUNCTION IF EXISTS public.music_string_mass_per_length(numeric, numeric, numeric, numeric);
+CREATE FUNCTION public.music_string_mass_per_length(
+	tension numeric,
+	frequency numeric,
+    length numeric,
+	n numeric DEFAULT 1
+) RETURNS numeric
+	AS 'select (tension/((frequency*2*length)/n)^2);'
+    LANGUAGE SQL
+    IMMUTABLE
+    RETURNS NULL ON NULL INPUT;
+
+-- Item#,Unit Weight,g',f',e',d',c',b,a,g
+-- PL009,.00001794,18.6,14.7,** 13.1 **,10.4,8.3,7.4,5.8,4.6
+-- tension   = 13.1 lb mass * 453.59237*980.665 to convert to ~Newtons (g*cm/s**2), from F=ma
+-- frequency = 329.63 Hz, i.e. E4 (e')
+-- length    = 25.5 inches * 2.54 to convert inches to cm
+-- 178.58    = needed to convert g/m to lb/in to check results against PL009 spec by Daadario
+SELECT public.music_string_mass_per_length(tension=>13.1*453.59237*980.665,frequency=>329.63, length => 25.5*2.54)/178.58;
+
+-- Item#,Unit Weight,g',f',e',d',c',b,a,g
+-- PL0095,.00001999,20.7,16.4,** 14.6 **,11.6,9.2,8.2,6.5,5.2
+-- tension   = 14.6 lb mass * 453.59237*980.665 to convert to ~Newtons (g*cm/s**2), from F=ma
+-- frequency = 329.63 Hz, i.e. E4 (e')
+-- length    = 25.5 inches * 2.54 to convert inches to cm
+-- 178.58    = needed to convert g/m to lb/in to check results against PL0095 spec by Daadario
+SELECT public.music_string_mass_per_length(tension=>14.6*453.59237*980.665,frequency=>329.63, length => 25.5*2.54)/178.58;
+
+-- Item#,Unit Weight,f',e',d',c',b,a,g,f
+-- J4301,.00002024,16.6,** 14.8 **,11.8,9.3,8.3,6.6,5.2,4.2
+-- tension   = 14.8 lb mass * 453.59237*980.665 to convert to ~Newtons (g*cm/s**2), from F=ma
+-- frequency = 329.63 Hz, i.e. E4 (e')
+-- length    = 25.5 inches * 2.54 to convert inches to cm
+-- 178.58    = needed to convert g/m to lb/in to check results against J4301 spec by Daadario
+SELECT public.music_string_mass_per_length(tension=>14.8*453.59237*980.665,frequency=>329.63, length => 25.5*2.54)/178.58;
+
+-- Item#,Unit Weight,f',e',d',c',b,a,g,f
+-- J4503/EXP,.00004679,38.4,** 34.2 **,27.2,21.6,19.2,15.2,12.1,9.6
+-- tension   = 34.2 lb mass * 453.59237*980.665 to convert to ~Newtons (g*cm/s**2), from F=ma
+-- frequency = 329.63 Hz, i.e. E4 (e')
+-- length    = 25.5 inches * 2.54 to convert inches to cm
+-- 178.58    = needed to convert g/m to lb/in to check results against J4301 spec by Daadario
+SELECT public.music_string_mass_per_length(tension=>34.2*453.59237*980.665,frequency=>329.63, length => 25.5*2.54)/178.58;
+
+-- Item#,Unit Weight,f',e',d',c',b,a,g,f
+-- J4503/EXP,.00004679,38.4,34.2,27.2,21.6,** 19.2 **,15.2,12.1,9.6
+-- tension   = 19.2 lb mass * 453.59237*980.665 to convert to ~Newtons (g*cm/s**2), from F=ma
+-- frequency = 246.9 Hz, i.e. B3 (b)
+-- length    = 25.5 inches * 2.54 to convert inches to cm
+-- 178.58    = needed to convert g/m to lb/in to check results against J4301 spec by Daadario
+SELECT public.music_string_mass_per_length(tension=>19.2*453.59237*980.665,frequency=>246.9, length => 25.5*2.54)/178.58;
 
 -- https://en.wikipedia.org/wiki/String_vibration
 DROP FUNCTION IF EXISTS public.music_frequencies_mpl(numeric, numeric, numeric);
