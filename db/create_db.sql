57ca7333b60e64a1aa266a2fc45a59b350b63372
Haisam K. Ido <haisam.ido@nasa.gov>
2021-12-31 14:12:44 -0500

    can now calculate frequency from notation E3 as an example relative to A4(440.0) which is configurable

--- db/create_db.sql
+++ db/create_db.sql
@@ -1096,7 +1096,21 @@ CREATE FUNCTION music.octave_difference(
   IMMUTABLE
   RETURNS NULL ON NULL INPUT;
 
-SELECT music.octave_difference('G3');
+-- return the number of intervals away from A4 which is needed to determine the frequency
+SELECT (
+	(SELECT music.octave_difference('G3'))*12 + 
+	(SELECT semitones_from_A4 from music.chromatic_scale where note=(( SELECT note from music.international_pitch_notations where notation='G3' ) ))
+); 
+
+SELECT music.frequency_of_interval(
+	n=>(
+		SELECT (
+			(SELECT music.octave_difference('A0'))*12 + 
+			(SELECT semitones_from_A4 from music.chromatic_scale where note=(( SELECT note from music.international_pitch_notations where notation='A0' ) ))
+		)
+	)
+);
+
 
 -- CREATE TABLE manufacturer_string_families (
 --   id SERIAL PRIMARY KEY,
