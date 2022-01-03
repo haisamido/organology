cdb6d10d19ef0b56119fedbe8072b385fc491543
Haisam K. Ido <haisam.ido@nasa.gov>
2022-01-02 19:54:55 -0500

    split up music.*.sql

--- db/insert_music.octaves.sql
+++ db/insert_music.octaves.sql
@@ -1,17 +1,4 @@
 
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('C',-9);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('C#',-8);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('D',-7);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('D#',-6);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('E',-5);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('F',-4);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('F#',-3);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('G',-2);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('G#',-1);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('A',0);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('A#',1);
-INSERT INTO music.chromatic_scale (note,semitones_from_A4) VALUES ('B',2);
-
 INSERT INTO music.octaves (octave_number,octave_name,midi_number) VALUES (-1,'subsubcontra',0);
 INSERT INTO music.octaves (octave_number,octave_name,midi_number) VALUES (0,'sub-contra',12);
 INSERT INTO music.octaves (octave_number,octave_name,midi_number) VALUES (1,'contra',24);
